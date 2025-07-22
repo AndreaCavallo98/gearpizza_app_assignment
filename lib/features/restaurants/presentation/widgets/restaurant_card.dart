f953c45ff146f553909c1e99c59b04115bb7f4df
AndreaCavallo98 <andrea.cavallo.pc@gmail.com>
2025-07-22 06:21:52 -0400

    restaurant page list

---
+++ lib/features/restaurants/presentation/widgets/restaurant_card.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class RestaurantCard extends StatelessWidget {
+  const RestaurantCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: double.maxFinite,
+          height: 210,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 15,
+                child: ClipPath(
+                  clipper: CustomCardClipper(),
+                  child: Image.network(
+                    "https://gearpizza.revod.services/assets/10205b8c-33ae-4c42-9f77-9f25b811e787?access_token=${dotenv.env['GUEST_ACCESS_TOKEN']}",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: CircleAvatar(
+                  radius: 32,
+                  backgroundColor: Colors.black,
+                  child: Icon(Icons.arrow_forward_ios, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Text(
+          "Pizza Napoletana",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+        Row(
+          children: [
+            // 5 stelle fai un ciclo for
+            ...List.generate(
+              4,
+              (index) => Icon(Icons.star, color: Colors.yellow),
+            ),
+            Icon(Icons.star, color: Colors.grey),
+            Spacer(),
+            Icon(Icons.location_on_outlined, color: Colors.grey),
+            Text("10 km", style: TextStyle(color: Colors.grey, fontSize: 16)),
+          ],
+        ),
+        SizedBox(height: 30),
+      ],
+    );
+  }
+}
+
+class CustomCardClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double radius = 30;
+    double curveRadius = 20;
+    Path path = Path();
+    path.moveTo(radius, 0);
+    path.lineTo(size.width - radius, 0);
+    path.quadraticBezierTo(size.width, 0, size.width, radius);
+    path.lineTo(size.width, size.height - (curveRadius + 60));
+    path.quadraticBezierTo(
+      size.width,
+      size.height - 60,
+      size.width - curveRadius,
+      size.height - 60,
+    );
+    path.quadraticBezierTo(
+      size.width - 70,
+      size.height - 65,
+      size.width - 75,
+      size.height - curveRadius,
+    );
+    path.quadraticBezierTo(
+      size.width - 75,
+      size.height,
+      size.width - (75 + curveRadius),
+      size.height,
+    );
+    path.lineTo(radius, size.height);
+
+    path.quadraticBezierTo(0, size.height, 0, size.height - radius);
+    path.lineTo(0, radius);
+    path.quadraticBezierTo(0, 0, radius, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gearpizza/core/di/injection.dart';
+import 'package:gearpizza/features/restaurants/presentation/widgets/restaurant_card.dart';
 import 'package:gearpizza/router/app_router.dart';
 
 void main() async {
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Gear Pizza',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: "poppins",
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
     );
@@ -33,45 +35,134 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = _counter + 2;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            // 👉 Header NON scrollabile
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "La tua posizione",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black45,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.orange,
+                              size: 20,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "Via Centallo 121",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    alignment: AlignmentDirectional.topCenter,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.shopping_cart_outlined,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                      ),
+                      Positioned(
+                        right: -5,
+                        top: -8,
+                        child: GestureDetector(
+                          child: Container(
+                            padding: EdgeInsets.all(7),
+                            decoration: BoxDecoration(
+                              color: Color(0xfff95f60),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Text(
+                              "0",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 35),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                "Trova la migliore pizza per te!",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  letterSpacing: -.4,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            // 👉 Scrollabile solo la lista delle RestaurantCard
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                itemCount: 4, // o restaurants.length
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: RestaurantCard(),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
